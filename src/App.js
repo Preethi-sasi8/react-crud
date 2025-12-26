5b3174084f60683b081b9d1f092b779b7928b860
Preethi-sasi8 <preethisasikumar811@gmail.com>
2025-12-27 02:43:28 +0530

    Displaying users data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+
+import { useState , useEffect} from 'react';
 import './App.css';
 
 function App() {
+
+   const [users,setusers] = useState([]);
+   const [name,setname] = useState([]);
+   const [email,setemail] = useState([]);
+   const [website,setwebsite] = useState([]);
+   useEffect(() => {
+  fetch("https://jsonplaceholder.typicode.com/users").then(resp => resp.json()).then(data => setusers(data))}, []);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+             <table>
+              <thead>
+                <th>ID</th>
+                <th>NAME</th>
+                <th>EMAIL</th>
+                <th>WEBSITE</th>
+                 <th>ACTION</th>
+              </thead>
+              <tbody>
+                {users.map(user =>
+                <tr key={user.id}>
+                  <td>{user.id}</td>
+                  <td>{user.name}</td>
+                  <td  contenteditable="true">{user.email}</td>
+                  <td  contenteditable="true">{user.website}</td>
+                <td>
+                 <button>UPDATE</button> 
+                 <button>DELETE</button> 
+                  </td></tr>
+                )}
+              </tbody>
+              <tfoot>
+                <td><input type="text" placeholder='enter name...' /></td>
+              </tfoot>
+             </table>
     </div>
   );
 }
